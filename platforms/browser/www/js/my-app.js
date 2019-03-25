dacc5377d0325c07dedd3b83d7d75582fc9471bd
paulchin <paulchin20@yahoo.com>
2019-03-25 17:28:39 +0800

    initial

---
+++ platforms/browser/www/js/my-app.js
@@ -0,0 +1,156 @@
+// Initialize app
+var myApp = new Framework7();
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+  console.log('Device is ready!');
+  initApp();
+});
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function(page) {
+  // Do something here for "about" page
+});
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function(e) {
+  // Get page data from event data
+  var page = e.detail.page;
+
+  if (page.name === 'about') {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Here comes About page');
+  }
+});
+
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function(e) {
+  // Following code will be executed for page with data-page attribute equal to "about"
+  myApp.alert('Here comes About page');
+});
+
+//------ Admob ----------
+var admobid = {
+  banner: 'ca-app-pub-3940256099942544/6300978111',
+  interstitial: 'ca-app-pub-3940256099942544/1033173712'
+};
+
+$$('#admobBanner').click(function() {
+  //createSelectedBanner();
+  AdMob.showBanner(AdMob.AD_POSITION.BOTTOM_CENTER);
+});
+
+$$('#admobInterstitial').click(function() {
+  //createInterstitial();
+  AdMob.showInterstitial();
+});
+
+function initApp() {
+  if (!AdMob) {
+    alert('admob plugin not ready');
+    return;
+  }
+
+  initAd();
+  // display a banner at startup
+  //createSelectedBanner();
+}
+
+function initAd() {
+  var defaultOptions = {
+    bannerId: admobid.banner,
+    interstitialId: admobid.interstitial,
+    adSize: 'SMART_BANNER',
+    // width: integer, // valid when set adSize 'CUSTOM'
+    // height: integer, // valid when set adSize 'CUSTOM'
+    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    // offsetTopBar: false, // avoid overlapped by status bar, for iOS7+
+    bgColor: 'black', // color name, or '#RRGGBB'
+    // x: integer,		// valid when set position to 0 / POS_XY
+    // y: integer,		// valid when set position to 0 / POS_XY
+    isTesting: true, // set to true, to receiving test ad for testing purpose
+    autoShow: false // auto show interstitial ad when loaded, set to false if prepare/show
+  };
+  AdMob.setOptions(defaultOptions);
+
+  registerAdEvents();
+  AdMob.prepareInterstitial({ adId: admobid.interstitial, autoShow: false });
+  AdMob.createBanner({
+    adId: admobid.banner,
+    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    autoShow: false
+  });
+}
+
+// optional, in case respond to events or handle error
+function registerAdEvents() {
+  // new events, with variable to differentiate: adNetwork, adType, adEvent
+  document.addEventListener('onAdFailLoad', function(data) {
+    alert(
+      'error: ' +
+        data.error +
+        ', reason: ' +
+        data.reason +
+        ', adNetwork:' +
+        data.adNetwork +
+        ', adType:' +
+        data.adType +
+        ', adEvent:' +
+        data.adEvent
+    ); // adType: 'banner' or 'interstitial'
+  });
+  document.addEventListener('onAdLoaded', function(data) {
+    alert(data.adType + ' ad loaded');
+  });
+  document.addEventListener('onAdPresent', function(data) {
+    alert(data.adType + ' ad present');
+  });
+  document.addEventListener('onAdLeaveApp', function(data) {
+    alert(data.adType + ' ad leave');
+  });
+  document.addEventListener('onAdDismiss', function(data) {
+    alert(data.adType + ' ad dismissed');
+    AdMob.prepareInterstitial({ adId: admobid.interstitial, autoShow: false });
+  });
+}
+
+function createSelectedBanner() {
+  // var overlap = document.getElementById('overlap').checked;
+  // var offsetTopBar = document.getElementById('offsetTopBar').checked;
+  // AdMob.createBanner({
+  //   adId: admobid.banner,
+  //   overlap: overlap,
+  //   offsetTopBar: offsetTopBar,
+  //   adSize: getSelectedAdSize(),
+  //   position: getSelectedPosition()
+  // });
+  AdMob.showBanner();
+}
+
+function createInterstitial() {
+  // var overlap = document.getElementById('overlap').checked;
+  // var offsetTopBar = document.getElementById('offsetTopBar').checked;
+  // AdMob.createBanner({
+  //   adId: admobid.banner,
+  //   overlap: overlap,
+  //   offsetTopBar: offsetTopBar,
+  //   adSize: getSelectedAdSize(),
+  //   position: getSelectedPosition()
+  // });
+  // AdMob.({
+  //   adId: admobid.interstitial,
+  //   autoShow: true
+  // });
+  AdMob.showInterstitial();
+}
